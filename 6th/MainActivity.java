a9fc4d884fa42cafc5aec741d0280b71360932b8
yujin-00 <dbwls7ska@gmail.com>
2020-05-10 16:24:15 +0900

    My First App

---
+++ 6th/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://yujin-00.github.io/"));
+        startActivity(intent);
+    }
+}
